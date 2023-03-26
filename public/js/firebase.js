abaed63f33c768483b9c6974f805e0d55ae8c228
Nihal <69518128+nihal549@users.noreply.github.com>
2023-03-26 11:05:59 +0530

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDgvHeqqKMXpLYrFJ29z1Ler7H6X8bRv8k",
+    authDomain: "majorproject-72f5a.firebaseapp.com",
+    projectId: "majorproject-72f5a",
+    storageBucket: "majorproject-72f5a.appspot.com",
+    messagingSenderId: "131239009603",
+    appId: "1:131239009603:web:94593b261d33fc91d3e3f6"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
